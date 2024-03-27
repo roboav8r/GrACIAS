eb33bc55423e2d6d12c937768393db782e982593
John Duncan <john.a.duncan@utexas.edu>
2024-03-27 17:57:08 -0500

    Update AR tag size, add update step in interaction manager

--- scripts/int_mgr_node.py
+++ scripts/int_mgr_node.py
@@ -4,6 +4,9 @@ import numpy as np
 
 import rclpy
 from rclpy.node import Node
+
+import gtsam
+
 from tracking_msgs.msg import Tracks3D
 from gracias_interfaces.msg import Auth
 from gracias_interfaces.msg import Comm 
@@ -14,12 +17,24 @@ from foxglove_msgs.msg import SceneUpdate, SceneEntity, TextPrimitive
 class Person():
     def __init__(self, msg):
         self.track_id = msg.track_id
-        self.identity = "Unknown identity"
-        self.auth = "Not authenticated"
+        self.identity = "Unknown"
+        self.auth = False
         self.comms = []
         self.pos_x = msg.pose.pose.position.x
         self.pos_y = msg.pose.pose.position.y
         self.pos_z = msg.pose.pose.position.z
+
+    def update(self, ar_msg, type):
+        if type=='authentication':
+            if ar_msg.authenticated:
+                self.auth = True
+
+        if type=='communication':
+            self.comms=[ar_msg.comm]
+
+        if type=='identity':
+            self.identity = ar_msg.identity
+
 
 class InteractionManagerNode(Node):
     def __init__(self):
@@ -66,12 +81,11 @@ class InteractionManagerNode(Node):
 
     def compute_match(self, pos):
         similarity_vector = np.zeros(len(self.persons.keys()))
-        self.get_logger().info('Similarity vector: "%s"' % similarity_vector)
 
-        for ii,key in enumerate(self.persons.keys()): [TODO come back here]
-            similarity_vector[ii] = np.linalg.norm(pos.x - self.persons[key].pos_x, pos.y - self.persons[key].pos_y, pos.z - self.persons[key].pos_z)
+        for ii,key in enumerate(self.persons.keys()): 
+            similarity_vector[ii] = np.linalg.norm([pos.x - self.persons[key].pos_x, pos.y - self.persons[key].pos_y, pos.z - self.persons[key].pos_z])
 
-        self.get_logger().info('Similarity vector: "%s"' % similarity_vector)
+        return list(self.persons.keys())[np.argmin(similarity_vector)]
 
     def listener_callback_tracked_persons(self, msg):
         self.tracks_msg = msg
@@ -82,9 +96,13 @@ class InteractionManagerNode(Node):
         for trkd_person in msg.tracks:
             self.tracked_person_ids.append(trkd_person.track_id)
 
-            # Manage dict of tracked people; key is track_id
+            # Create person and add to dict if not currently tracked
             if trkd_person.track_id not in self.persons.keys():
                 self.persons[trkd_person.track_id] = Person(trkd_person)
+
+            self.persons[trkd_person.track_id].pos_x = trkd_person.pose.pose.position.x
+            self.persons[trkd_person.track_id].pos_y = trkd_person.pose.pose.position.y
+            self.persons[trkd_person.track_id].pos_z = trkd_person.pose.pose.position.z
 
         # remove untracked persons
         persons_temp = self.persons
@@ -96,26 +114,24 @@ class InteractionManagerNode(Node):
     def listener_callback_auth(self, msg):
         # self.get_logger().info('Received auth message: "%s"' % msg)
 
-        # Compute match
-        self.compute_match(msg.pose.pose.position)
-
-        # Perform update
+        # Compute match, perform update
+        match_key = self.compute_match(msg.pose.pose.position)
+        self.persons[match_key].update(msg, 'authentication')
 
     def listener_callback_comm(self, msg):
         # self.get_logger().info('Received communication message: "%s"' % msg)
 
-        # Compute match
-        self.compute_match(msg.pose.pose.position)
-
-        # Perform update
+        # Compute match, perform update
+        match_key = self.compute_match(msg.pose.pose.position)
+        self.persons[match_key].update(msg, 'communication')
 
     def listener_callback_identity(self, msg):
         # self.get_logger().info('Received identity message: "%s"' % msg)
 
-        # Compute match
-        self.compute_match(msg.pose.pose.position)
+        # Compute match, perform update
+        match_key = self.compute_match(msg.pose.pose.position)
+        self.persons[match_key].update(msg, 'identity')
 
-        # Perform update
 
     def visualize(self):
         self.scene_msg = SceneUpdate()
@@ -129,19 +145,20 @@ class InteractionManagerNode(Node):
             entity_msg.timestamp = self.tracks_msg.header.stamp
             entity_msg.id = str(person.track_id)
             entity_msg.frame_locked = True
-            entity_msg.lifetime.nanosec = 3000000
+            entity_msg.lifetime.nanosec = 500000000
 
             text = TextPrimitive()
             text.billboard = True
-            text.font_size = 24.
+            text.font_size = 16.
             text.scale_invariant = True
             text.color.a = 1.0
             text.pose.position.x = person.pos_x
             text.pose.position.y = person.pos_y
             text.pose.position.z = person.pos_z
-            text.text = "person #%s: \n%s \n%s" % (person.track_id, person.identity, person.auth)
+            text.text = "person #%s: \nID: %s \nAuth: %s" % (person.track_id, person.identity, person.auth)
             for msg in person.comms:
                 text.text += "\n%s" % (msg)
+            text.text
             entity_msg.texts.append(text)
 
             self.scene_msg.entities.append(entity_msg)
