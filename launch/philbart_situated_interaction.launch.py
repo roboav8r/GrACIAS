32de0324149918c932ee45af10c46bea7cda6048
John Duncan <john.a.duncan@utexas.edu>
2024-08-20 20:50:41 -0500

    Launch scene rec nodes

--- launch/philbart_situated_interaction.launch.py
+++ launch/philbart_situated_interaction.launch.py
@@ -14,20 +14,25 @@ from launch_ros.descriptions import ComposableNode
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # Config files
+
+    ### CONFIG FILES
     config = os.path.join(
         get_package_share_directory('situated_interaction'),
         'config',
         'philbart_config.yaml'
     )
 
+
+    ### TF DATA
     tf_node = Node(package = "tf2_ros", 
                     executable = "static_transform_publisher",
                     arguments = [".15", "0", ".55", "0", "-0.261799", "0", "philbart/base_link", "oak-d-base-frame"]
     )
     ld.add_action(tf_node)
 
-    # ROS2 Sensor nodes
+
+    ### SENSORS
+    # Vision
     cam_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
@@ -50,6 +55,39 @@ def generate_launch_description():
     )
     ld.add_action(acq_node)
 
+
+    ### UNIMODAL PROCESSING
+    # Scene recognition - vision
+    clip_rec_node = Node(package = "mm_scene_rec", 
+                    executable = "clip_scene_rec.py",
+                    name = "clip_scene_rec",
+                    remappings=[('/clip_scene_image','/oak/rgb/image_raw')],
+                    parameters=[config]
+    )
+    ld.add_action(clip_rec_node)
+    
+    # Scene recognition - audition
+    audio_rec_node = Node(
+        package='mm_scene_rec',
+        executable='audio_scene_rec.py',
+        name='audio_scene_rec',
+        output='screen',
+        parameters=[config]
+    )
+    ld.add_action(audio_rec_node)
+
+
+    ### MULTIMODAL PROCESSING
+    # Scene recognition
+    scene_rec_node = Node(package = "mm_scene_rec", 
+                    executable = "bayes_scene_est.py",
+                    name = "bayes_fused_scene_est",
+                    parameters=[config]
+    )
+    ld.add_action(scene_rec_node)
+
+
+    ### OUTPUT / VISUALIZATION
     # Foxglove bridge for visualization
     viz_node = IncludeLaunchDescription(
         XMLLaunchDescriptionSource(
