3ff4cb790c3d13d779b127e2e2acf62bbc506bc5
John Duncan <john.a.duncan@utexas.edu>
2024-08-23 19:00:17 -0500

    Add pointcloud to laserscan

--- launch/philbart_situated_interaction.launch.py
+++ launch/philbart_situated_interaction.launch.py
@@ -55,6 +55,28 @@ def generate_launch_description():
     )
     ld.add_action(acq_node)
 
+    # LiDAR to laserscan node
+    lidar_to_scan = Node(
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in', 'philbart/lidar_points'),
+                        ('scan', 'philbart/scan')],
+            parameters=[{
+                'target_frame': 'cloud',
+                'transform_tolerance': 0.01,
+                'min_height': 0.0,
+                'max_height': 1.0,
+                'angle_min': -1.5708,  # -M_PI/2
+                'angle_max': 1.5708,  # M_PI/2
+                'angle_increment': 0.0087,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 4.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        )
+    ld.add_action(lidar_to_scan)
 
     ### UNIMODAL PROCESSING
     # Scene recognition - vision
