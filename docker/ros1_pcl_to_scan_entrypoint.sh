5e219914b17f3f246716fc541ae86ea217477792
John Duncan <john.a.duncan@utexas.edu>
2024-08-25 17:22:23 -0500

    Add pcl_to_scan in dr_spaam package for Docker

--- docker/ros1_pcl_to_scan_entrypoint.sh
+++ docker/ros1_pcl_to_scan_entrypoint.sh
@@ -7,7 +7,5 @@ source "/opt/ros/$ROS1_DISTRO/setup.bash"
 cd /ros1_ws 
 source devel/setup.bash 
 
-# Display bridged messages and launch the bridge
-# TODO - adjust ROS parameters
-rosrun pointcloud_to_laserscan pointcloud_to_laserscan_node cloud_in:=$PCL_TOPIC scan:=$SCAN_TOPIC
+roslaunch dr_spaam_ros philbart_pcl_to_laserscan.launch
 exec "$@"
